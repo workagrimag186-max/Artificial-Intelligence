1582f37942db3e4550373a7c336c55486d74a414
Agrima Gupta <work.agrimag186@gmail.com>
2026-08-21 03:14:24 +0530

    Add BestFirstSearch prgm

---
+++ BestFirstSearch.py
@@ -0,0 +1,53 @@
+import heapq
+
+graph={
+    'S':['A','B'],
+    'A':['C','D'],
+    'B':['E'],
+    'C':[],
+    'D':['F'],
+    'E':['G'],
+    'F':[],
+    'G':[]
+}
+
+h={
+    'S':6,
+    'A':4,
+    'B':5,
+    'C':2,
+    'D':3,
+    'E':4,
+    'F':1,
+    'G':0
+}
+
+queue=[(h['S'],'S')]
+
+visited=set()
+
+while queue:
+
+    _,node=heapq.heappop(queue)
+
+    if node not in visited:
+
+        print(node,end=" ")
+
+        visited.add(node)
+
+        if node=='G':
+            print("\nGoal!!")
+            break
+
+        neighbours=[]
+
+        for neighbour in graph[node]:
+
+            if neighbour not in visited:
+
+                neighbours.append(neighbour)
+
+        for neighbour in neighbours:
+
+            heapq.heappush(queue,(h[neighbour],neighbour))
